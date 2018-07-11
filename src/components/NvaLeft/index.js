9471ed650682198aa2cf20e8896133cc5069f877
xnng <xnng77@gmail.com>
2018-07-11 20:09:36 +0800

    完成惨淡渲染

--- src/components/NvaLeft/index.js
+++ src/components/NvaLeft/index.js
@@ -2,19 +2,28 @@ import React from 'react';
 import MenuConfig from '../../config/menueConfig'
 import { Menu, Icon } from 'antd';
 import './index.css'
+
 const SubMenu = Menu.SubMenu;
 
-
 export default class NvaLeft extends React.Component {
-    componentWillMount(){
+    componentWillMount() {
         const MenuTreeNode = this.renderMenu(MenuConfig);
         this.setState({
             MenuTreeNode
         });
     }
 
-    renderMenu = (data) =>{
-        
+    renderMenu = (data) => {
+        return data.map((item) => {
+            if (item.children) {
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item key={item.key}>{item.title}</Menu.Item>
+        })
     }
 
     render() {
@@ -24,13 +33,7 @@ export default class NvaLeft extends React.Component {
                     <img src="./assets/logo-ant.svg" alt="" />
                     <h1>Space MS</h1>
                 </div>
-                <Menu theme="dark" mode="inline">
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                        <Menu.Item key="1">Option 1</Menu.Item>
-                        <Menu.Item key="2">Option 2</Menu.Item>
-                        <Menu.Item key="4">Option 3</Menu.Item>
-                        <Menu.Item key="4">Option 4</Menu.Item>
-                    </SubMenu>
+                <Menu theme="dark" mode="vertical">
                     {this.state.MenuTreeNode}
                 </Menu>
             </div>
